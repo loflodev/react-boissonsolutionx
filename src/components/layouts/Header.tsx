1cfa594ebcb6ec3e5312e827826969b0f2ea2d32
Louis Florival <ljunit25@icloud.com>
2025-06-10 00:51:41 -0400

    first commit

---
+++ src/components/layouts/Header.tsx
@@ -0,0 +1,96 @@
+// interface HeaderProps {}
+
+const Header = () => {
+  return (
+    <header className="bg-white dark:bg-gray-900">
+      <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
+        <div className="flex h-22 items-center justify-between">
+          <div className="md:flex md:items-center md:gap-12">
+            <a className="block text-teal-600 dark:text-teal-600" href="#">
+              <span className="sr-only">Home</span>
+              <img
+                className="w-[180px] md:w-auto"
+                src="/bsx-logo.webp"
+                alt="Boisson Solution X Logo"
+              />
+            </a>
+          </div>
+
+          <div className="hidden md:block ml-auto">
+            <nav aria-label="Global">
+              <ul className="flex items-center gap-12 text-sm">
+                <li>
+                  <a
+                    className="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75"
+                    href="#"
+                  >
+                    Acceuil
+                  </a>
+                </li>
+
+                <li>
+                  <a
+                    className="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75"
+                    href="#"
+                  >
+                    Produits
+                  </a>
+                </li>
+
+                <li>
+                  <a
+                    className="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75"
+                    href="#"
+                  >
+                    À propos de nous
+                  </a>
+                </li>
+
+                <li>
+                  <a
+                    className="text-gray-500 transition hover:text-gray-500/75 dark:text-white dark:hover:text-white/75"
+                    href="#"
+                  >
+                    Où acheter
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+
+          <div className="flex items-center gap-4 ml-12">
+            <div className="sm:flex sm:gap-4">
+              <a
+                className="rounded-full bg-secondary px-8 py-2.5 text-lg font-medium text-dark shadow-sm dark:hover:bg-btn-hover"
+                href="#"
+              >
+                Contact
+              </a>
+            </div>
+
+            <div className="block md:hidden">
+              <button className="rounded-sm bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75 dark:bg-gray-800 dark:text-white dark:hover:text-white/75">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="size-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
